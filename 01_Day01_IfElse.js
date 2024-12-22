af2420da438ec90933ee59c28dcff14b2eeda3f2
Ashok <63443149+aashokrao@users.noreply.github.com>
2024-12-22 08:55:07 +0530

    Add files via upload
    Week01_Day01_Assignment_If-Else conditions

---
+++ 01_Day01_IfElse.js
@@ -0,0 +1,12 @@
+
+function launchBrowser(browserName){
+    if(browserName==="chrome"){
+        console.log("Input browser name is:"+browserName)
+    }else if(browserName==="firefox"){
+        console.log("Input browser name is:"+browserName)
+    }else{
+        console.log("Please check the input browser name:"+browserName)
+    }
+}
+
+launchBrowser("edge")
